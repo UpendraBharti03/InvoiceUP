118f7f7188f617045b1c07135174b0d19095077a
Upendra Bharti <110711823+UpendraBharti03@users.noreply.github.com>
2024-05-08 11:25:32 +0530

    added config for validating env-vars if needed in future

---
+++ api/src/config/config.ts
@@ -0,0 +1,24 @@
+import * as process from 'process';
+//@ts-ignore
+import moduleAlias from "module-alias";
+import path from 'path';
+import dotenv from 'dotenv';
+
+moduleAlias({
+    base: path.join(__dirname, '../../')
+})
+
+dotenv.config({ path: path.join(__dirname, '../../.env') });
+
+const envVars = process.env;
+
+export const config = {
+    env: envVars.NODE_ENV,
+    port: envVars.PORT,
+    mongoose: {
+        url: envVars.MONGODB_URL ?? '',
+    },
+    jwt: {
+        secret: envVars.JWT_SECRET,
+    }
+}
