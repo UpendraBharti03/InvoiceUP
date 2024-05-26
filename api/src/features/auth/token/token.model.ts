be6e13f139e82c9539bf7ecdeaba6f50d89230de
UpendraBharti03 <upendrabharti74@gmail.com>
2024-05-26 13:19:27 +0530

    api: added TTL in token model to delete token from db after expires

--- api/src/features/auth/token/token.model.ts
+++ api/src/features/auth/token/token.model.ts
@@ -10,13 +10,13 @@ export class IToken {
     @prop({type: mongoose.SchemaTypes.ObjectId, required: true, ref: modelsInfo?.USER?.modelName})
     public userId!: mongoose.Types.ObjectId;
     
-    @prop({type: String, required: true})
+    @prop({type: String, required: true, index: true})
     public token!: string;
 
     @prop({type: String, required: true, enum: Object.values(tokenTypes)})
     public tokenType!: TTokenType;
 
-    @prop({type: Date, required: true})
+    @prop({type: Date, required: true, expires: 120})
     public expires!: Date;
 }
 
