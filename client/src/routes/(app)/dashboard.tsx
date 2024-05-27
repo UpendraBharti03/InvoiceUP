548904c41a67c871bf3b2dfa7ef77ab1186fcf97
UpendraBharti03 <upendrabharti74@gmail.com>
2024-05-27 22:04:43 +0530

    client: resolved redux initial state issue with tanstack router

--- client/src/routes/(app)/dashboard.tsx
+++ client/src/routes/(app)/dashboard.tsx
@@ -2,13 +2,15 @@
 import DashboardPage from '@/components/dashboard/DashboardPage'
 import { authenticate } from '@/layout/auth/authenticate';
 import { AUTH_SLICE_NAME } from '@/redux/slices/authSlice';
+import { store } from '@/redux/store';
 import { createFileRoute, redirect } from '@tanstack/react-router'
 // import { useSelector } from 'react-redux';
 
 export const Route = createFileRoute('/(app)/dashboard')({
   beforeLoad: ({ context }) => {
-    const auth = context[AUTH_SLICE_NAME];
-    console.log("auth--,,,-->>>>", auth)
+    // we have to reassign auth slice state in route context
+    context.auth = store.getState()[AUTH_SLICE_NAME]
+    const auth = context.auth;
     authenticate({ auth, isAuthenticatedRoute: true });
   },
   component: () => <DashboardPage />
--- client/src/routes/(auth)/signup.tsx
+++ client/src/routes/(auth)/signup.tsx
@@ -1,14 +1,17 @@
 import SignupPage from '@/components/signup/SignupPage'
 import { authenticate } from '@/layout/auth/authenticate';
 import { AUTH_SLICE_NAME } from '@/redux/slices/authSlice';
+import { store } from '@/redux/store';
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(auth)/signup')({
     beforeLoad: ({ context }) => {
-        const auth = context[AUTH_SLICE_NAME];
+        // we have to reassign auth slice state in route context
+        context.auth = store.getState()[AUTH_SLICE_NAME]
+        const auth = context.auth;
         authenticate({ auth, isAuthenticatedRoute: false });
-      },
+    },
     component: () => (
-            <SignupPage />
+        <SignupPage />
     )
 })
