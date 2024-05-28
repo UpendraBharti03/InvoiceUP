b34e1fbac75d0cced9682950a7b86731acdc2e8b
Upendra Bharti <110711823+UpendraBharti03@users.noreply.github.com>
2024-05-28 19:38:24 +0530

    panel layout drafted work and added initial profile page for demo

--- client/src/layout/panelLayout/PanelLayout.tsx
+++ client/src/layout/panelLayout/PanelLayout.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 
 type TPanelLayoutProps = {
     children: React.ReactNode;
 }
 
 function PanelLayout({ children }: TPanelLayoutProps) {
-    return <>{children}</>;
+    return <>
+    <Layout style={{ minHeight: '100vh' }}>
+    <Layout.Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+    </Layout.Sider>
+    <Layout>
+    <Layout.Content style={{ margin: '0 16px' }}></Layout.Content>
+    {children}
+    </Layout>
+    </Layout>
+    </>;
 }
 
 export default PanelLayout;
