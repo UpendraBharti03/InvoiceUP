3747dbc748d868d1653ce704652e128edb6c025b
UpendraBharti03 <upendrabharti74@gmail.com>
2024-06-10 13:11:21 +0530

    client: minor ui changes for invoice preview

--- client/src/components/invoice/preview/InvoicePreview.tsx
+++ client/src/components/invoice/preview/InvoicePreview.tsx
@@ -29,8 +29,9 @@ export const InvoicePreview = ({ invoiceDetails }: { invoiceDetails: TInvoiceZS 
             </div>
 
             {/* Product items */}
-            <div className={"grid grid-cols-5 border-y"}>
+            <div className={"grid grid-cols-6 border-y"}>
                 <>
+                    <div className={"font-semibold truncate"}>S. No.</div>
                     <div className={"font-semibold truncate"}>Product</div>
                     <div className={"font-semibold truncate"}>Qty</div>
                     <div className={"font-semibold truncate"}>Unit price</div>
@@ -41,6 +42,7 @@ export const InvoicePreview = ({ invoiceDetails }: { invoiceDetails: TInvoiceZS 
                     const amount = productItem?.quantity * (productItem?.unitPrice - (productItem?.unitPrice * (productItem?.discount ?? 0)) / 100)
                     return (
                         <React.Fragment key={productItemIndex}>
+                            <div className={"truncate"}>{productItemIndex + 1}</div>
                             <div className={"truncate"}>{productItem?.product?.productName}</div>
                             <div className={"truncate"}>{productItem?.quantity}</div>
                             <div className={"truncate"}>{productItem?.unitPrice}</div>
@@ -52,15 +54,17 @@ export const InvoicePreview = ({ invoiceDetails }: { invoiceDetails: TInvoiceZS 
             </div>
 
             <div className={"flex justify-end"}>
-            <div className={"grid grid-cols-2 max-w-[300px]"}>
-                <div className={"border-b pr-2 truncate"}>Price:</div>
-                <div className={"border-b truncate"}>{invoiceDetails?.totalPrice}</div>
-                <div className={"border-b pr-2 truncate"}>Tax rate:</div>
-                <div className={"border-b truncate"}>{invoiceDetails?.taxRate}</div>
-                <div className={"font-semibold pr-2 truncate"}>Total amount:</div>
-                <div className={"font-semibold truncate"}>{invoiceDetails?.totalAmount}</div>
+                <div className={"grid grid-cols-2 max-w-[300px]"}>
+                    <div className={"border-b pr-2 truncate"}>Price:</div>
+                    <div className={"border-b truncate"}>{invoiceDetails?.totalPrice}</div>
+                    <div className={"border-b pr-2 truncate"}>Tax rate:</div>
+                    <div className={"border-b truncate"}>{invoiceDetails?.taxRate}</div>
+                    <div className={"font-semibold pr-2 truncate"}>Total amount:</div>
+                    <div className={"font-semibold truncate"}>{invoiceDetails?.totalAmount}</div>
+                </div>
             </div>
-            </div>
+
+            <div>{invoiceDetails?.invoiceDescription}</div>
         </div>
     );
 }
