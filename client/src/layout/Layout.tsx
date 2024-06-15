64809978b2a825905ac4b4905a0f1e666d3893aa
UpendraBharti03 <upendrabharti74@gmail.com>
2024-06-15 12:18:45 +0530

    bugfix(client): get-profile api was getting called after other api calling, which is now resolved

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -1,15 +1,24 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useMemo } from "react";
 import PanelLayout from "@/layout/panelLayout/PanelLayout";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/redux/store";
 import { getUserProfile } from "@/redux/slices/authSlice";
+import { LoadingOutlined } from '@ant-design/icons';
+import { Flex, Spin } from "antd";
 
 const CustomLayout = ({children}: {children: ReactNode}) => {
     const dispatch = useDispatch<AppDispatch>();
 
-    useEffect(() => {
-        dispatch(getUserProfile());
-    }, [])
+    const profile = useMemo(() => {
+        const profile = dispatch(getUserProfile());
+        return profile;
+    }, []);
+
+    if (!profile) {
+        <Flex className={"h-screen w-full bg-color-bg-layout"} justify={"center"} align={"center"}>
+                <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />
+            </Flex>
+    }
     // Other layouts will appear here with condition (for future use)
     // if (userType === .....)
 
